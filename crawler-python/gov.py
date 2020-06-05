083d2eae3f73016665a6aab6e40cdd1f6afc38a4
Aleksander Surman <osurman@gmail.com>
2020-06-06 01:53:17 +0200

    added translating

--- crawler-python/gov.py
+++ crawler-python/gov.py
@@ -1,7 +1,8 @@
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup, Comment
 import urllib.request
-import json
 import pyrebase
+import copy
+from googletrans import Translator
 
 config = {
   "apiKey": "AIzaSyDOVEnFxl7AXDjlxzrgDzJbIEmN2I7770I",
@@ -10,8 +11,26 @@ config = {
   "storageBucket": "project-532228461078.appspot.com"
 }
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(tag):
+    texts = tag.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    # return u" ".join(t.strip() for t in visible_texts)
+    return list(filter(lambda a : a != "", [t.strip() for t in visible_texts]))
+
+srcLang = "pl"
+destLangs = ["sk", "cs", "hu", "en"]
 
 if __name__ == '__main__':
+    translator = Translator()
+
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
 
@@ -27,10 +46,27 @@ if __name__ == '__main__':
     for current in sectionsIterator:
         headerTag = current
         descriptionTag = next(sectionsIterator)
+
         section = {}
         section["id"] = counter
-        section["title"] = headerTag.text
-        section["content"] = descriptionTag.prettify()
+        section["title"] = {}
+        section["title"][srcLang] = headerTag.text
+        section["content"] = {}
+        section["content"][srcLang] = descriptionTag.prettify()
+
+        for destLang in destLangs:
+            copyHeaderTag = copy.copy(headerTag)
+            copyDescriptionTag = copy.copy(descriptionTag)
+
+            for txt in copyDescriptionTag.findAll(text=True):
+                if txt.strip() == "":
+                    continue
+                print("#"+txt+"#")
+                txt.replaceWith(translator.translate(str(txt), dest=destLang, src=srcLang).text)
+
+            section["title"][destLang] = translator.translate(str(copyHeaderTag), dest=destLang, src=srcLang).text
+            section["content"][destLang] = copyDescriptionTag.prettify()
+
         sections += [section]
         counter += 1
 
