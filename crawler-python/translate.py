7031411b174d74a802f1d3ab264a34d959ad36c2
Konrad Kraszewski <laiquendir@gmail.com>
2020-06-06 03:23:28 +0200

    Use google translate lib.

--- crawler-python/translate.py
+++ crawler-python/translate.py
@@ -1,8 +1,10 @@
 import copy
 import time
 from googletrans import Translator
+from google.cloud import translate_v2 as translate
 
-translator = Translator()
+#translator = Translator()
+translate_client = translate.Client()
 
 destLangs = [
 #    "sk",
@@ -22,7 +24,13 @@ def translate(srcLang, element):
         for txt in copyElement.findAll(text=True):
             if txt.strip() == "":
                 continue
-            txt.replaceWith(translator.translate(str(txt), dest=destLang, src=srcLang).text)
+            #txt.replaceWith(translator.translate(str(txt), dest=destLang, src=srcLang).text)
+            txt.replaceWith(
+              translate_client.translate(
+                str(txt).decode('utf-8'),
+                source_language=srcLang,
+                target_language=destLang)['translatedText']
+            )
     
         output[destLang.upper()] = copyElement.prettify()
     print(output)
