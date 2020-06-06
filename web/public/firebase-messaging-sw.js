8c048a55cad1c0b92852546dec9d739c86b09320
Jakub Król <jakub.krol@mc2innovations.com>
2020-06-07 01:23:33 +0200

    push text update

--- web/public/firebase-messaging-sw.js
+++ web/public/firebase-messaging-sw.js
@@ -1,12 +1,6 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
   apiKey: "AIzaSyAO1d2EL3qFxCAIUi-T7j3FfS9sSzcmSmg",
   authDomain: "hy-s-2020.firebaseapp.com",
@@ -17,21 +11,18 @@ firebase.initializeApp({
   appId: "1:532228461078:web:639107890f0a378f60db37"
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
-
 
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
 
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  if (payload.data.type === 'change-cat') {
+    const notificationTitle = 'Change of restrictions';
+    const notificationOptions = {
+      body: 'Warning! There has been a change in restrictions. Go to the highlighted category to learn more.',
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+  }
 });
 
