a9f642097a11ed310d9547c73d5b294857688bb1
Jakub Król <jakub.krol@mc2innovations.com>
2020-06-06 13:10:16 +0200

    header layout update

--- web/src/components/general-info/Numbers.vue
+++ web/src/components/general-info/Numbers.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="d-flex flex-column text-right mr-4">
-    <div class="title">
-      <span>{{ icon }} {{ value }}</span>
+    <div class="title ">
+      {{ icon }}  <span :class="colorClass">{{ changes }}</span>
     </div>
-    <div class="body-2 font-weight-bold" :class="colorClass">
-      {{ changes }}
+    <div class="body-1 total-info" >
+      {{ value }}
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['icon', 'value', 'changes'],
+    props: ['icon', 'value', 'changes', 'inverted-color'],
     data () {
       return {
       };
     },
     computed: {
+      positiveSign() {
+        return this.invertedColor ? '+' : '-';
+      },
+      negativeSign() {
+        return this.invertedColor ? '-' : '+';
+      },
       colorClass () {
-        if (this.changes[0] === '-') {
+        if (this.changes[0] === this.positiveSign) {
           return 'text-green';
-        } else if (this.changes[0] === '+') {
+        } else if (this.changes[0] === this.negativeSign) {
           return 'text-red';
         } else {
           return '';
@@ -31,6 +37,10 @@
 </script>
 
 <style scoped>
+  .total-info {
+    margin-top: -6px;
+  }
+
   .text-green {
     color: green;
   }
--- web/src/components/general-info/Panel.vue
+++ web/src/components/general-info/Panel.vue
@@ -3,13 +3,13 @@
     <v-row class="body-2">
       {{ country }}
     </v-row>
-    <v-row class="subtitle-1">
+    <v-row class="subtitle-1 primary--text font-weight-medium">
       {{ region }}
     </v-row>
     <v-row>
       <div class="d-flex flex-row pt-2">
         <Numbers icon="😷" :value="3123" changes="+15" />
-        <Numbers icon="💪" :value="1504" changes="-5" />
+        <Numbers icon="💪" :value="1504" changes="+5" :inverted-color="true" />
         <Numbers icon="💀" :value="120" changes="+8" />
       </div>
     </v-row>
